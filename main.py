0f479d0c1039ed8a19a81fbbe1520e5adca5587b
blchrd <thomas3.blanchard3@gmail.com>
2023-07-29 12:15:32 -0600

    Write html transformation of common format

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import csv
+import json
 
 CSV_FILE_NAME = "data.csv"
 
@@ -11,7 +12,31 @@ def common_format():
   return data
 
 def to_html(common_fmt):
-  pass
+  html = """
+  <table>
+    <caption>
+      Revenue by year
+    </caption>
+    <thead>
+      <tr>
+        <th>Index</th>
+        <th>Year</th>
+        <th>Revenue</th>
+      <tr>
+    </thead>
+    <tbody>"""
+  for (index, row) in enumerate(common_fmt):
+    html += """
+    <tr>
+      <td>{0}</td>
+      <td>{1}</td>
+      <td>{2}</td>
+    </tr>""".format(index+1, row["Year"], row["Revenue"])
+  html += "</tbody>"
+  html += "</table>"
+
+  return html
+
 
 def to_svg(common_fmt):
   pass
