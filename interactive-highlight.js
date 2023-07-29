ab8e0f27407c9ae5330f09dcf66a4551dd2ee7ce
Tait Hoyem <tait@tait.tech>
2023-07-29 14:50:14 -0600

    Interactive highlgith updatates

--- interactive-highlight.js
+++ interactive-highlight.js
@@ -1,12 +1,28 @@
+//const websocketServer = "ws://localhost:3000/ws";
+//
+//const highlightSocket = new WebSocket(
+//    websocketServer,
+//);
+
 const highlight_table_focus = (ev) => {
   const item = ev.target;
   item.classList.toggle("highlight")
   console.log(ev)
 }
 
-document.querySelectorAll(".highlightable")
+const highlight_svg_click = (ev) => {
+  const bar = ev.target;
+  bar.classList.toggle("highlight")
+  const id = Number(bar.id.replace("bar-", ""))
+  highlightSocket.send(JSON.stringify({index: id, action: "highlight"}))
+}
+
+document.querySelectorAll("table tr")
   .forEach((item) => {
-    console.log(item)
-    item.addEventListener("focus", highlight_table_focus)
     item.addEventListener("click", highlight_table_focus)
+  })
+
+document.querySelectorAll("svg rect")
+  .forEach((item) => {
+    item.addEventListener("click", highlight_svg_click)
   })
