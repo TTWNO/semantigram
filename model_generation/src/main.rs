392ed52a3ff0b98d9408093165e623fb1473c6d2
Tait Hoyem <tait@tait.tech>
2023-07-31 19:54:23 -0600

    Use askama instead of tera

--- model_generation/src/main.rs
+++ model_generation/src/main.rs
@@ -1,32 +1,58 @@
 use std::{error::Error, io::BufReader, fs::File};
 use serde::{Serialize, Deserialize};
-use tera::{Tera, Context};
+use ::csv::Reader;
+use askama::Template;
+
+#[derive(Clone, Debug, Template)]
+#[template(path = "partials/table.html")]
+struct HtmlTableTemplate {
+	records: Vec<Record>,
+	caption: String,
+}
+
+#[derive(Clone, Debug, Template)]
+#[template(path = "partials/svg.svg", escape = "none")]
+pub struct SvgTemplate {
+	pub x_axis_size: i32,
+	pub y_axis_size: i32,
+	pub x_width: i32,
+	pub padding: i32,
+	pub outline_color: String,
+	pub fill_color: String,
+	pub largest_value: i32,
+	pub records: Vec<Record>,
+}
+
+mod filters {
+    pub fn to_i32(idx: &usize) -> ::askama::Result<i32> {
+			Ok(*idx as i32)
+    }
+}
 
 // TODO: replace with generic "dataframe"-like structure.
 // we only ever can store one label, with two data points:
 // an x and y axis.
 #[derive(Clone, Debug, Serialize, Deserialize)]
-struct Record {
+pub struct Record {
 	pub year: i32,
 	pub revenue: i32,
 }
 
 fn main() -> Result<(), Box<dyn Error>>{
-	let mut tera = Tera::new("templates/**/*").unwrap();
-	let mut ctx = Context::new();
-
 	let file = File::open("../data.csv")?;
-	let mut reader = BufReader::new(file);
-	let mut rdr = csv::Reader::from_reader(reader);
+	let reader = BufReader::new(file);
+	let mut rdr = Reader::from_reader(reader);
 	let mut all_rows = Vec::new();
 	for result in rdr.deserialize() {
 		let record: Record = result?;
 		all_rows.push(record);
 	}
-	println!("{:?}", all_rows);
-	ctx.insert("caption", "Revenue by Year");
-	ctx.insert("records", &all_rows);
-	let rendered = tera.render("table.html", &ctx).unwrap();
-	println!("{rendered}");
+	
+	let table = HtmlTableTemplate {
+		records: all_rows,
+		caption: "Revenue by Year".to_string(),
+	};
+
+	println!("{}", &table.render()?);
 	Ok(())
 }
