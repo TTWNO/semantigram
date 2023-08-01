7acda9afa08408dc67b9778e9410083e3c45c993
Tait Hoyem <tait@tait.tech>
2023-07-31 23:06:36 -0600

    Cargo formatting

--- model_generation/src/main.rs
+++ model_generation/src/main.rs
@@ -1,48 +1,50 @@
-use std::{error::Error, io::BufReader, fs::File};
-use serde::{Serialize, Deserialize};
 use ::csv::Reader;
 use askama::Template;
+use serde::{Deserialize, Serialize};
+use std::{error::Error, fs::File, io::BufReader};
 
 #[derive(Clone, Debug, Template)]
 #[template(path = "partials/table.html")]
 pub struct HtmlTableTemplate {
-	records: Vec<Record>,
-	caption: String,
+    records: Vec<Record>,
+    caption: String,
 }
 
 #[derive(Clone, Debug, Template)]
 #[template(path = "partials/svg.svg", escape = "none")]
 pub struct SvgTemplate {
-	pub x_axis_size: i32,
-	pub y_axis_size: i32,
-	pub x_width: i32,
-	pub padding: i32,
-	pub outline_color: String,
-	pub fill_color: String,
-	pub largest_value: i32,
-	pub records: Vec<Record>,
+    pub x_axis_size: i32,
+    pub y_axis_size: i32,
+    pub x_width: i32,
+    pub padding: i32,
+    pub outline_color: String,
+    pub fill_color: String,
+    pub largest_value: i32,
+    pub records: Vec<Record>,
 }
 
 mod filters {
-	pub fn to_i32<T>(idx: &T) -> ::askama::Result<i32> 
-		where i32: TryFrom<T>,
-		<i32 as TryFrom<T>>::Error: std::fmt::Debug,
-		T: Copy {
-		Ok(i32::try_from(*idx).unwrap())
-	}
-	pub fn to_f64(i: &i32) -> ::askama::Result<f64> {
-		Ok(f64::try_from(*i).unwrap())
-	}
-	pub fn f_to_i32(i: &f64) -> ::askama::Result<i32> {
-		Ok((*i).round() as i32)
-	}
+    pub fn to_i32<T>(idx: &T) -> ::askama::Result<i32>
+    where
+        i32: TryFrom<T>,
+        <i32 as TryFrom<T>>::Error: std::fmt::Debug,
+        T: Copy,
+    {
+        Ok(i32::try_from(*idx).unwrap())
+    }
+    pub fn to_f64(i: &i32) -> ::askama::Result<f64> {
+        Ok(f64::try_from(*i).unwrap())
+    }
+    pub fn f_to_i32(i: &f64) -> ::askama::Result<i32> {
+        Ok((*i).round() as i32)
+    }
 }
 
 #[derive(Clone, Debug, Template)]
 #[template(path = "alpha.html")]
 pub struct AlphaTemplate {
-	pub svg: SvgTemplate,
-	pub table: HtmlTableTemplate,
+    pub svg: SvgTemplate,
+    pub table: HtmlTableTemplate,
 }
 
 // TODO: replace with generic "dataframe"-like structure.
@@ -50,40 +52,43 @@ pub struct AlphaTemplate {
 // an x and y axis.
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Record {
-	pub year: i32,
-	pub revenue: i32,
+    pub year: i32,
+    pub revenue: i32,
 }
 
-fn main() -> Result<(), Box<dyn Error>>{
-	let file = File::open("../data.csv")?;
-	let reader = BufReader::new(file);
-	let mut rdr = Reader::from_reader(reader);
-	let mut all_rows = Vec::new();
-	for result in rdr.deserialize() {
-		let record: Record = result?;
-		all_rows.push(record);
-	}
-	
-	let table = HtmlTableTemplate {
-		records: all_rows.clone(),
-		caption: "Revenue by Year".to_string(),
-	};
-	let largest_value = all_rows.iter().max_by_key(|r| r.revenue).unwrap().revenue.clone().into();
-	let svg = SvgTemplate {
-		records: all_rows,
-		x_axis_size: 1000.into(),
-		y_axis_size: 1000.into(),
-		x_width: 50.into(),
-		padding: 25.into(),
-		outline_color: "none".to_string(),
-		fill_color: "lightblue".to_string(),
-		largest_value
-	};
-	let alpha = AlphaTemplate {
-		svg,
-		table
-	};
+fn main() -> Result<(), Box<dyn Error>> {
+    let file = File::open("../data.csv")?;
+    let reader = BufReader::new(file);
+    let mut rdr = Reader::from_reader(reader);
+    let mut all_rows = Vec::new();
+    for result in rdr.deserialize() {
+        let record: Record = result?;
+        all_rows.push(record);
+    }
 
-	println!("{}", &alpha.render()?);
-	Ok(())
+    let table = HtmlTableTemplate {
+        records: all_rows.clone(),
+        caption: "Revenue by Year".to_string(),
+    };
+    let largest_value = all_rows
+        .iter()
+        .max_by_key(|r| r.revenue)
+        .unwrap()
+        .revenue
+        .clone()
+        .into();
+    let svg = SvgTemplate {
+        records: all_rows,
+        x_axis_size: 1000.into(),
+        y_axis_size: 1000.into(),
+        x_width: 50.into(),
+        padding: 25.into(),
+        outline_color: "none".to_string(),
+        fill_color: "lightblue".to_string(),
+        largest_value,
+    };
+    let alpha = AlphaTemplate { svg, table };
+
+    println!("{}", &alpha.render()?);
+    Ok(())
 }
