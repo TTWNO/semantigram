0d763f481e4841b08b648bf607ddbb97dbf8be19
Tait Hoyem <tait@tait.tech>
2023-09-30 15:10:34 -0600

    It works!

--- websocket-client.js
+++ websocket-client.js
@@ -43,7 +43,7 @@ highlightSocket.onmessage = (event) => {
               selector = `[data-col="${coordinates}"]`;
               break;
           case "cell":
-              selector = `[data-col="${coordinates[0]}"][data-row="${coordinates[1]}"], [data-col="${coordinates[0]}"]:not([data-row]), [data-row="${coordinates[1]}"]:not([data-col])`;
+              selector = `[data-col="${coordinates[0]}"][data-row="${coordinates[1]}"], [data-col="${coordinates[0]}"].highlight:not([data-row]), [data-row="${coordinates[1]}"].highlight:not([data-col])`;
               break;
           default:
               break;
