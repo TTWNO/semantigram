45a56ab763c5a38934c04ce71b5f7edea8192417
blchrd <thomas3.blanchard3@gmail.com>
2023-09-30 12:48:55 -0600

    [WIP] change websocket client with new data structure

--- websocket-client.js
+++ websocket-client.js
@@ -1,11 +1,17 @@
 const ws_proto = window.location.protocol === "https:" ? "wss" : "ws";
 const hostname = window.location.hostname === "" ? "localhost:3000" : window.location.hostname;
-// message will be {'index': 0, 'action': 'highlight'}
+// highlight / unhighlight
 const HighlightElement = Object.freeze({
-    TABLE: "table",
-    GRAPHBAR: "bar",
+    HTML: "table",
+    GRAPHIC: "bar",
 });
-const fromTo = {'table': 'bar', 'bar': 'table'};
+const ElementState = Object.freeze({
+    HIGHLIGHTED: "highlight",
+    HOVERED: "hovered",
+    NONE: "",
+});
+
+const fromTo = { 'table': 'bar', 'bar': 'table' };
 const websocketServer = ws_proto + "://" + hostname + "/ws";
 console.log("Connect to ", websocketServer);
 
@@ -16,40 +22,61 @@ const highlightSocket = new WebSocket(
 highlightSocket.onopen = (event) => {
     let username = (Math.random() + 1).toString(36).substring(7);
     // send message with username
-    highlightSocket.send(username)
+    highlightSocket.send(username);
 };
 
 highlightSocket.onmessage = (event) => {
-    console.log(event.data);
-    const datas = JSON.parse(event.data);
-    const index = datas["index"];
+    // {"position": {"type": "row", "data": [2]}, "action": "highlight"} // [null, 2]
+    // {"position": {"type": "column", "data": [2]}, "action": "highlight"} // [2, null]
+    // {"position": {"type": "cell", "data": [0,2]}, "action": "highlight"}
 
-    console.log(HighlightElement.GRAPHBAR+"-"+index);
-    let barItem = document.getElementById(HighlightElement.GRAPHBAR+"-"+index);
-    let tableItem = document.getElementById(HighlightElement.TABLE+"-"+index);
+    console.log(event.data);
+
+    const datas = JSON.parse(event.data);
+    const type = datas["position"]["type"];
+    const coordinates = datas["position"]["data"]
+    const action = datas["action"]
+
+    switch (type) {
+        case "row":
+            break;
+        case "column":
+            break;
+        case "cell":
+            break;
+        default:
+            break;
+    }
+
+    console.log(HighlightElement.GRAPHIC + "-" + index);
+
+    let typeItem;
+    let barItem = document.getElementById(HighlightElement.GRAPHIC + "-" + index);
+    let tableItem = document.getElementById(HighlightElement.HTML + "-" + index);
     var children = tableItem.children;
 
-		const is_enabled = !!children[3].children[0].checked;
-		const log = document.querySelector("#a11y-log");
-		if (is_enabled) {
-			const text = "row " + index + " has been unselected\n";
-			console.log(text);
-			log.innerHTML = text;
-		} else {
-			const text = "row " + index + " has been selected\n";
-			console.log(text);
-			log.innerHTML = text;
-		}
+    const is_enabled = !!children[3].children[0].checked;
+    const log = document.querySelector("#a11y-log");
+    if (is_enabled) {
+        const text = "row " + index + " has been unselected\n";
+        console.log(text);
+        log.innerHTML = text;
+    } else {
+        const text = "row " + index + " has been selected\n";
+        console.log(text);
+        log.innerHTML = text;
+    }
 
-    barItem.classList.toggle("highlight");
+    barItem.classList.toggle(ElementState.HIGHLIGHTED);
     for (var i = 0; i < children.length; i++) {
         var tableChild = children[i];
-        tableChild.classList.toggle("highlight");
+        tableChild.classList.toggle(ElementState.HIGHLIGHTED);
     }
-		// toggle checkbox
-		children[3].children[0].checked = !children[3].children[0].checked;
+
+    // toggle checkbox
+    children[3].children[0].checked = !children[3].children[0].checked;
 };
 
 function highlightItem(index, highlight) {
-    highlightSocket.send(`{"index": ${index}, "action": "highlight"}`);    
+    highlightSocket.send(`{"index": ${index}, "action": ${ElementState.HIGHLIGHTED}}`);
 };
