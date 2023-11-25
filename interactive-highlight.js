5873fe993d9565d4c40bbe0a0161a7d3cd784835
Tait Hoyem <tait@tait.tech>
2023-11-25 14:24:29 -0700

    Add focus/unfocus event handlers, on top of highlgihting

--- interactive-highlight.js
+++ interactive-highlight.js
@@ -1,14 +1,26 @@
 const highlight_element = (ev) => {
   const element = ev.target;
+
+  action = "";
+  switch (ev.type) {
+    case "change":
+    case "click":
+      action = element.classList.contains("highlight") ? "unhighlight" : "highlight";
+      break;
+    case "mouseover":
+    case "focusin":
+      action = "focus";
+      break;
+    case "mouseleave":
+    case "focusout":
+      action = "unfocus";
+      break;
+  }
+
   const x_row = element.getAttribute('data-row');
   const x_col = element.getAttribute('data-col');
   let elementType = "cell";
   let coordinates = [parseInt(x_col), parseInt(x_row)];
-  let action = "highlight";
-
-  if (element.classList.contains("highlight")) {
-     action = "unhighlight"
-  }
 
   if (x_col === null) {
     elementType = "row";
@@ -26,5 +38,7 @@ const highlight_element = (ev) => {
 document.querySelectorAll("[data-row],[data-col]")
   .forEach((item) => {
     item.addEventListener("click", highlight_element)
+    item.addEventListener("change", highlight_element)
+    item.addEventListener("mouseover", highlight_element)
+    item.addEventListener("mouseleave", highlight_element)
   })
-  
